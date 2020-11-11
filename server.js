46fe3bafd6c3d5498d63cddbf12f5abe06764917
Wendy Grant <wendykgrant.tx@gmail.com>
2020-11-10 18:41:03 -0600

    started server.js

--- server.js
+++ server.js
@@ -3,3 +3,14 @@ const express = require('express');
 const logger = require('morgan');
 const mongoose = require('mongoose');
 
+// setup port
+const PORT = process.env.PORT || 3000;
+
+// setup logging of incoming information
+app.use(logger("dev"));
+
+// parse incoming requests
+app.use(express.urlencoded ({ extended:true }));
+app.use(express.json());
+
+// 
