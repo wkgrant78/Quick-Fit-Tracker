443d52d3fdd98761c4b62e283a82c330cf19ac38
Wendy Grant <wendykgrant.tx@gmail.com>
2020-11-13 09:52:32 -0600

    testing connection

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ mongoose.connect(
     process.env.MONGODB_URI || 'mongodb://localhost/workout',
     {
     useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
+    // useUnifiedTopology: true,
+    // useCreateIndex: true,
     useFindAndModify: false
     }
 );
