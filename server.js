f9089950510628757d421f0c7976efd80a849d04
Wendy Grant <wendykgrant.tx@gmail.com>
2020-11-12 18:02:43 -0600

    deploying

--- server.js
+++ server.js
@@ -15,7 +15,15 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParser: true});
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/workout',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
 
 
 //routes used for CRUD execution
